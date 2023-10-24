097b4b7d5a495494ab143e08a84ea6a4e89d8cba
Anson Li <ansonli@fundpark.com>
2023-10-24 16:40:03 +0800

    for sl

--- elo/constants.py
+++ elo/constants.py
@@ -1,5 +1,6 @@
 source = '/Users/ansonli/go/elo/LIHKG-Record.xlsx'
 out_source = '/Users/ansonli/go/elo/LIHKG-Record-with-Elo.xlsx'
+csv_out_path = '/Users/ansonli/go/elo/'
 rank_elo_map = {
     '12k': 900,
     '8k': 1300,
